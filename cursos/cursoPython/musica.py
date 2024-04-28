2b4a6cc0eb61338c4311ce638cb2173beed327af
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2024-04-28 19:36:26 -0300

    Finalizado modulo de url e validacoes

--- cursos/cursoPython/musica.py
+++ cursos/cursoPython/musica.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Musica:
     def __init__(self, nome, cantor, genero):
@@ -9,6 +9,18 @@ class Musica:
 
 musica01 = Musica('Pais e Filho', 'Legião Urbana', 'MPB')
 lista = [musica01]
+
+class Usuario:
+    def __init__(self, nome, login, senha):
+        self.nome = nome
+        self.login = login
+        self.senha = senha
+
+usuario01 = Usuario('Lorenzo', 'lnunez', 'lnunez')
+
+usuarios = {
+    usuario01.login : usuario01
+}
 
 app = Flask(__name__)
 
@@ -23,22 +35,25 @@ def autenticar():
     usuario = request.form['user']
     senha = request.form['password']
 
-    if usuario == 'admin':
+    if usuario in usuarios and usuarios[usuario].senha == senha:
         session['usuario_logado'] = usuario
-        flash("Usuário logado com sucesso!")
-        return redirect('/')
+        flash(f"Usuário {usuario} logado com sucesso!")
+        return redirect(url_for('listarMusicas'))
     else:
         flash("Usuário ou senha inválidos!")
-        return redirect('/login')
+        return redirect(url_for('login'))
 
 @app.route('/sair')
 def sair():
     session['usuario_logado'] = None
 
-    return redirect('/')
+    return redirect(url_for('login'))
 
 @app.route('/cadastrar')
 def cadastrarMusicas():
+
+    if session['usuario_logado'] == None or 'usuario_logado' not in session:
+        return redirect(url_for('login'))
     return render_template('cadastrar_musicas.html', title = "Cadastrar Música")
 
 @app.route('/adicionar', methods = ['POST',])
@@ -51,10 +66,13 @@ def adicionarMusica():
 
     lista.append(novaMusica)
 
-    return redirect('/')
+    return redirect(url_for('listarMusicas'))
 
 @app.route('/')
 def listarMusicas():
+    if session['usuario_logado'] == None or 'usuario_logado' not in session:
+        return redirect(url_for('login'))
+
     return render_template('lista_musicas.html', title = "Musicas Cadastradas", musicas = lista)
 
 app.run(debug=True)
